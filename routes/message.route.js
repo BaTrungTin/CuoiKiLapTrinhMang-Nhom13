a27d1cb9c5bf8664003114d0032bc1dbee788ae1
Hggg_Ringgg <trinhhdm2798@ut.edu.vn>
2025-08-30 15:48:16 +0700

    routes fix

--- routes/message.route.js
+++ routes/message.route.js
@@ -3,14 +3,18 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getUsersForSidebar,
   sendMessage,
+  deleteMessage,
+  getUnreadCounts,
 } from "../controllers/message.controller.js";
 import { getMessages } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/user", protectRoute, getUsersForSidebar);
+router.get("/unread", protectRoute, getUnreadCounts);
 router.get("/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);
+router.delete("/:messageId", protectRoute, deleteMessage);
 
 export default router;
